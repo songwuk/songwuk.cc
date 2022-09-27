cec1210d31b2ab2af104d0e127e22bf5be6bf669
songwuk <1017301325@qq.com>
2022-09-27 20:25:01 +0800

    feat: add more

---
+++ src/components/WriteTyperContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="writetyper"></div>
+</template>
+<script setup lang="ts">
+
+import { onMounted } from 'vue';
+import { UnTyper } from 'untyper'
+
+export interface Props {
+  labelsText?: string
+}
+const props = withDefaults(defineProps<Props>(),{
+  labelsText: ''
+})
+const languageCode = {
+  'en-US':  {
+    text: 'Hi I&apos;m becoming indie developer !'
+  },
+  'zh-CN': {
+    text: '嗨，我正在成为独立开发者 !'
+  },
+  'fr-ca': {
+    text: 'Bonjour, je deviens développeur indépendant !'
+  },
+  'ko': {
+    text: '안녕하세요 인디 개발자가 되고 있습니다 !'
+  }
+}
+onMounted(()=> {
+  const text = document.querySelector('.writetyper') as HTMLElement || null
+  const unTyper = new UnTyper(text, { speed: 100, startDelay: 0 })
+  unTyper.type(props.labelsText, { delay: 200 }).go()
+})
+</script>
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'What I should do?';
+export const SITE_TITLE = 'Songwuk - Homepage';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const HOMEPAGE_URL = 'https://www.songwuk.cc'
