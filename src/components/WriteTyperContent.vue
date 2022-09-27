d0532d77e1389b1fc94527f3e3ea559892b3ca58
songwuk <1017301325@qq.com>
2022-09-27 23:34:24 +0800

    feat: add post

--- src/components/WriteTyperContent.vue
+++ src/components/WriteTyperContent.vue
@@ -7,28 +7,42 @@ import { onMounted } from 'vue';
 import { UnTyper } from 'untyper'
 
 export interface Props {
-  labelsText?: string
+  textStatus?: boolean
 }
 const props = withDefaults(defineProps<Props>(),{
-  labelsText: ''
+  textStatus: true
 })
-const languageCode = {
-  'en-US':  {
-    text: 'Hi I&apos;m becoming indie developer !'
-  },
-  'zh-CN': {
-    text: '嗨，我正在成为独立开发者 !'
-  },
+const languageCode:Record<string,any> = {
   'fr-ca': {
     text: 'Bonjour, je deviens développeur indépendant !'
   },
   'ko': {
     text: '안녕하세요 인디 개발자가 되고 있습니다 !'
-  }
+  },
+  'zh-CN': {
+    text: '嗨，我正在成为独立开发者 !'
+  },
+  'en-US':  {
+    text: 'Hi I am becoming indie developer !'
+  },
 }
-onMounted(()=> {
+onMounted(async ()=> {
   const text = document.querySelector('.writetyper') as HTMLElement || null
-  const unTyper = new UnTyper(text, { speed: 100, startDelay: 0 })
-  unTyper.type(props.labelsText, { delay: 200 }).go()
+  const unTyper = new UnTyper(text, { speed: 100, startDelay: 2000 })
+  if(props.textStatus) {
+    const keys = Object.keys(languageCode) as string[];
+    let i = 0
+    keys.forEach(async (key) => {
+      i++
+      const textLine = languageCode[key].text
+      const len = textLine.length
+      if(i === keys.length){
+        unTyper.type(textLine,{ delay: 1000 })
+      } else {
+        unTyper.type(textLine,{ delay: 1000 }).delete(len)
+      }
+    });
+    unTyper.go()
+  }
 })
 </script>
