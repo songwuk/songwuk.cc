aa4d6a3f8dfd4ab6d27312f8eaf7c05a62af955f
songwuk <1017301325@qq.com>
2022-10-15 18:30:51 +0800

    docs: update

--- src/components/WriteTyperContent.vue
+++ src/components/WriteTyperContent.vue
@@ -13,27 +13,26 @@ const props = withDefaults(defineProps<Props>(),{
   textStatus: true
 })
 const languageCode:Record<string,any> = {
-  // 'zh-CN': {
-  //   text: '嗨，我正在成为独立<span style="color: rgb(253,186,116)">开发者 !</span>'
-  // },
+  'zh-CN': {
+    text: '嗨，我正在成为独立<span style="color: rgb(253,186,116)">开发者 !</span>'
+  },
   'en-US':  {
     text: 'Hi I&apos; becoming indie <span style="color: rgb(253,186,116)">developer !</span>'
   },
 }
 onMounted(async ()=> {
   const text = document.querySelector('.writetyper') as HTMLElement || null
-  const unTyper = new UnTyper(text, { speed: 100, startDelay: 2000 })
+  const unTyper = new UnTyper(text, { speed: 100, startDelay: 1000 })
   if(props.textStatus) {
     const keys = Object.keys(languageCode) as string[];
     let i = 0
     keys.forEach(async (key) => {
       i++
       const textLine = languageCode[key].text
-      const len = textLine.length
       if(i === keys.length){
         unTyper.add(textLine,{ delay: 100 })
       } else {
-        unTyper.add(textLine,{ delay: 100 })
+        unTyper.add(textLine,{ delay: 100 }).delete(15)
       }
     });
     unTyper.go()
