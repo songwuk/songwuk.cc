032c46f6282df7574cb8681f107033625ab45dd9
songwuk <1017301325@qq.com>
2022-11-04 18:33:36 +0800

    feat: upgrade canvas animate

---
+++ src/components/CanvasText.vue
@@ -0,0 +1,36 @@
+<template>
+  <canvas ref="canvas" w-full h-80></canvas>
+</template>
+<script setup lang="ts">
+  import { ref, onMounted} from 'vue'
+  // let attrsin = reactive<Record<string, object>>({
+  //   attrs: {}
+  // })
+  // const attrs = useAttrs()
+  // attrsin.attrs = attrs
+  const canvas = ref<HTMLCanvasElement | null>(null)
+  onMounted(() => {
+    const ctx = canvas.value!.getContext('2d')
+    const width = canvas.value!.width
+    const height = canvas.value!.height
+    const bgData = Array.from(new Array(400)).map(v => {
+      return {
+        x: Math.random() * width,
+        y: Math.random() * height,
+        step: Math.random() * 1.0 + 0.5
+      }
+    })
+    const render = () => {
+      ctx!.beginPath()
+      ctx!.fillStyle = '#ffffff'
+      ctx!.clearRect(0, 0, width, height)
+      bgData.forEach(v => {
+        v.y = v.y > height ? 0 : (v.y + v.step)
+        ctx!.rect(v.x, v.y, 2, 2)
+      })
+      ctx!.fill()
+      requestAnimationFrame(render)
+    }
+    render()
+  })
+</script>
