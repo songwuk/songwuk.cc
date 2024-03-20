9fdbcad35c2ba138a059177695d4440281939903
songwuk <1017301325@qq.com>
2024-03-20 08:47:56 +0800

    upgrade

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,1 +1,12 @@
-import { createFetch } from '@vueuse/core'
+export const TOKEN = "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"
+import { Octokit } from "octokit";
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({ auth: TOKEN});
+// Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
+// Octokit.js
+const res = await octokit.request('GET /licenses', {
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+console.log(res)
